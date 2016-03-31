6fcfe5abc156419b52427cad5ea4fe409c9e758f
Romain Edelmann <romain.edelmann@gmail.com>
2016-03-31 14:58:47 +0200

    Switched to non-concurrent implementation (to initally give to student?).

--- src/main/scala/pubsub/collection/BoundedBuffer.scala
+++ src/main/scala/pubsub/collection/BoundedBuffer.scala
@@ -2,6 +2,27 @@ package pubsub.collection
 
 import instrumentation.Schedulable
 
+class BoundedBuffer[T](size: Int) extends AbstractBoundedBuffer[T](size) {
+
+  override def put(e: T): Unit = {
+    buffer(tail) = e
+    count += 1
+  }
+
+  override def take(): T = {
+    val ret = buffer(head)
+    buffer.delete(head)
+    head = (head + 1) % size
+    count -= 1
+    ret
+  }
+
+  def tail: Int = (head + count) % size
+  def isFull: Boolean = count == size
+  def isEmpty: Boolean = count == 0
+}
+
+
 trait InternalBuffer[T] {
   def update(index: Int, elem: T): Unit
   def apply(index: Int): T
@@ -31,129 +52,4 @@ abstract class AbstractBoundedBuffer[T](bufferSize: Int) extends Schedulable {
 
   private var _head = 0;
   private var _count = 0;
-}
-
-class BoundedBuffer[T](size: Int) extends AbstractBoundedBuffer[T](size) {
-
-
-  def putWrong1(e: T): Unit = {
-    while (isFull) {      
-    }
-    buffer(tail) = e
-    count += 1
-  }
-
-  def takeWrong1(): T = {
-    while (isEmpty) {      
-    }
-    val toReturn = buffer(head)
-    buffer.delete(head)
-    head = (head + 1) % size
-    count -= 1
-    toReturn
-  }
-
-  def putWrong2(e: T): Unit = {
-    while (isFull) { }
-    buffer(tail) = e
-    count += 1
-  }
-  
-
-  /*def takeWrong2(): T = {
-    while (isEmpty) {
-      Thread.sleep(1)
-    }
-    lock.synchronized {
-      val toReturn = buffer(head)
-      buffer.delete(head)
-      head = (head + 1) % size
-      count -= 1
-      toReturn
-    }
-  }
-
-  def putWrong3(e: T): Unit = {
-    lock.synchronized {
-      while (isFull) {
-        Thread.sleep(1)
-      }
-      buffer(tail) = e
-      count += 1
-    }
-  }
-
-  def takeWrong3(): T = {
-    lock.synchronized {
-      while (isEmpty) {
-        Thread.sleep(1)
-      }
-      val toReturn = buffer(head)
-      buffer.delete(head)
-      head = (head + 1) % size
-      count -= 1
-      toReturn
-    }
-  }*/
-
-  // Correct versions.
-  override def put(e: T): Unit = {
-    this.synchronized {
-      while (isFull) {
-        this.wait()
-      }
-      buffer(tail) = e
-      count += 1
-      this.notifyAll()
-    }
-  }
-
-  override def take(): T = {
-    this.synchronized {
-      while (isEmpty) {
-        this.wait()
-      }
-      val toReturn = buffer(head)
-      buffer.delete(head)
-      head = (head + 1) % size
-      count -= 1
-      this.notifyAll()
-      toReturn
-    }
-  }
-  
-  def putCorrect2(e: T): Unit = {
-    while (true)  {
-      while (isFull) {}
-      synchronized {
-        if(!isFull) {
-          buffer(tail) = e
-          count += 1
-          return
-        }
-      }
-    }
-  }
-  
-  def takeCorrect2(): T = {
-    while (true) {
-      while (isEmpty) {}
-      synchronized {
-        if (!isEmpty) {
-          val toReturn = buffer(head)
-          buffer.delete(head)
-          head = (head + 1) % size
-          count -= 1
-          return toReturn
-        }
-      }
-    }
-    throw new Exception("Not implemented !!")
-  }
-
-  def tail: Int = (head + count) % size
-  def isFull: Boolean = count == size
-
-  def isEmpty: Boolean = count == 0
-
 }
